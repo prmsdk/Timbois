b1e0a665372f05d846ac5cb842d3273b099e9d9a
YogaE41181231 <ichayoga1@gmail.com>
2019-11-24 15:33:25 +0700

    keranjang

---
+++ ejsc/keranjang.php
@@ -0,0 +1,36 @@
+<?php
+require 'includes/header_content.php';
+?>
+
+<form>
+    <div class="card">
+        <div class="card-header">
+            <h3>Keranjang</h3>
+        </div>
+        <div class="card-body">
+            <h4>Nama Mitra</h4>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                <label class="form-check-label" for="defaultCheck1">
+                File.pdf x1
+                <h4>50.00</h4>
+                </label>
+            </div>
+        </div>
+        <hr>
+        <div class="card-body">
+            <h4>Nama Mitra</h4>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                <label class="form-check-label" for="defaultCheck1">
+                File.pdf x1
+                <h4>50.00</h4>
+                </label>
+            </div>
+        </div>
+
+    </div>
+</form>
+<?php
+    require 'includes/footer.php';
+?>
